c7bf4061b70e53ac6c75d6221485bfe9c7ee9f03
limmiehoang <minhkhanhhoang2105@gmail.com>
2018-10-09 08:11:42 +0700

    edit predict.py file

--- src/model/predict.py
+++ src/model/predict.py
@@ -14,16 +14,14 @@ X = dataset.iloc[:, :6000].values
 y = dataset.iloc[:, 6000].values
 
 # Splitting the dataset into the Training set and Test set
-# from sklearn.model_selection import train_test_split
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 # Feature Scaling
 # sc = StandardScaler()
 # X_train = sc.fit_transform(X_train)
 # X_test = sc.transform(X_test)
 
-X_test = dataset.iloc[1, :6000].values
-y_test = dataset.iloc[1,6000].values
 
 # Load the classifier
 with open(cf.TRAINED_MODEL, 'rb') as fid:
